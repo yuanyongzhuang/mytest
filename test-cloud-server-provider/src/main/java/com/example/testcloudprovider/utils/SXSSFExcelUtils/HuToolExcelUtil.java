a238b7cd1ac9f2e384096cf2ce75aedd6275a009
yuanyongzhuang <yuanyongzhuang>
2021-12-11 23:06:39 +0800

    导出测试dome

--- test-cloud-server-provider/src/main/java/com/example/testcloudprovider/utils/SXSSFExcelUtils/HuToolExcelUtil.java
+++ test-cloud-server-provider/src/main/java/com/example/testcloudprovider/utils/SXSSFExcelUtils/HuToolExcelUtil.java
@@ -2,6 +2,7 @@ package com.example.testcloudprovider.utils.SXSSFExcelUtils;
 
 import cn.afterturn.easypoi.excel.annotation.Excel;
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.poi.excel.BigExcelWriter;
 import cn.hutool.poi.excel.ExcelUtil;
 import cn.hutool.poi.excel.ExcelWriter;
@@ -26,34 +27,14 @@ import java.util.stream.Collectors;
 public class HuToolExcelUtil {
 
     /**
+     * 非合并单元格
      * @param clazz        实体类型
      * @param dataList     数据集合
      * @param sheetName     sheet名
      * @param destFilePath 文件路径
      */
     public static void exportBigExcel(Class clazz, List<?> dataList, String destFilePath, String sheetName) {
-        if(!dataList.isEmpty()){
-            if(dataList.get(0).getClass().equals(clazz)) {
-                BigExcelWriter bigWriter = ExcelUtil.getBigWriter(destFilePath);
-                //甚至sheet的名称
-                bigWriter.renameSheet(sheetName);
-
-                //设置Excel表头
-                setTheHeader(clazz, bigWriter);
-
-                bigWriter.write(dataList, true);
-                // 设置所有列为自动宽度，不考虑合并单元格
-                bigWriter.autoSizeColumnAll();
-                //输出别名
-                bigWriter.setOnlyAlias(true);
-                bigWriter.close();
-                log.info("导出完成!");
-            } else {
-                log.error("数据类型与传入的集合数据类型不一致！数据类型：{}; 集合数据类型：{}", clazz, dataList.get(0).getClass());
-            }
-        }else{
-            log.error("数据集合为空，数据类型：{}", clazz);
-        }
+        exportNeedMergeBigExcel(clazz,dataList,null,destFilePath,sheetName);
     }
 
     /**
@@ -73,29 +54,8 @@ public class HuToolExcelUtil {
 
             //设置Excel表头
             List<ExcelHeader> mergeList = setTheMergeHeader(clazz, bigWriter);
-
-            System.out.println("设置Excel表头:返回合并的表头==" + mergeList);
-
-            for(Map.Entry<Integer, Long> listEntry: collect.entrySet()){
-                Long value = listEntry.getValue();
-                //根据条数合并单元格
-                if(value == 1){
-                    //一条数据不合并
-                    for(ExcelHeader excelHeader: mergeList){
-                        excelHeader.setLineNum(Math.toIntExact(excelHeader.getLineNum() + value));
-                    }
-                }else{
-                    System.out.println("合并列：合并单元列数=="+mergeList.size());
-                    //合并
-                    for(ExcelHeader excelHeader: mergeList){
-                        System.out.println("合并列：合并行数=="+value);
-                        bigWriter.merge(excelHeader.getLineNum(),
-                                Math.toIntExact(excelHeader.getLineNum() + value - 1),
-                                excelHeader.getSortNum(), excelHeader.getSortNum(), null,true);
-                        excelHeader.setLineNum(Math.toIntExact(excelHeader.getLineNum() + value));
-                    }
-                }
-            }
+            //设置合并行
+            mergeRow(collect, bigWriter, mergeList);
 
             System.out.println("导出添加数据=="+dataList.size());
 
@@ -115,49 +75,37 @@ public class HuToolExcelUtil {
     }
 
     /**
-     * 设置Excel表头
-     * @param clazz 映射实体
-     * @param bigWriter
+     * 设置合并行
+     * @param collect 合并参数
+     * @param bigWriter 工具类
+     * @param mergeList 合并表头
      */
-    private static void setTheHeader(Class clazz, BigExcelWriter bigWriter) {
-        //获取当前类字段
-        Field[] fields = clazz.getDeclaredFields();
-        //字段中文名称集合（获取注解@Excel的name和orderNum排序值）集合存储
-        List<ExcelHeader> headerList = new ArrayList<>(fields.length);
-        for(Field field: fields){
-            //强制访问，越过安全检查
-            field.setAccessible(true);
-
-            //获取注解属性和字段名
-            boolean annotationPresent = field.isAnnotationPresent(Excel.class);
-            if(annotationPresent){
-                Excel excel = field.getAnnotation(Excel.class);
-//                HashMap<String, String> fileNameMap = new HashMap<>();
-//                fileNameMap.put(field.getName(),excel.name());
-//                ExcelHeader header = new ExcelHeader().setSortNum(Integer.valueOf(excel.orderNum())).setHeaderMap(fileNameMap);
-                ExcelHeader header = new ExcelHeader()
-                        .setSortNum(Integer.valueOf(excel.orderNum()))
-                        .setHeaderName(excel.name())
-                        .setParamName(field.getName())
-                        .setNeedMerge(excel.needMerge());
-                headerList.add(header);
-            }
-        }
-        System.out.println("无序表头设置："+headerList);
-        //表头排序
-        List<ExcelHeader> sortHeaders = headerList.stream().sorted(Comparator.comparing(ExcelHeader::getSortNum)).collect(Collectors.toList());
-        System.out.println("有序表头设置："+sortHeaders);
-        //设置表头
-        for (ExcelHeader header : sortHeaders){
-            if(header != null){
-                bigWriter.addHeaderAlias(header.getParamName(), header.getHeaderName());
-//                for(Map.Entry<String, String> entry : header.getHeaderMap().entrySet()){
-//                    //设置head的名称， 此时的顺寻就是导出的顺序， key就是映射实体的属性名称， value就是别名
-//                    bigWriter.addHeaderAlias(entry.getKey(), entry.getValue());
-//                }
+    private static void mergeRow(Map<Integer, Long> collect, BigExcelWriter bigWriter, List<ExcelHeader> mergeList) {
+        System.out.println("设置Excel表头:返回合并的表头==" + mergeList);
+        if(CollectionUtil.isNotEmpty(mergeList) || CollectionUtil.isNotEmpty(collect)) {
+            for (Map.Entry<Integer, Long> listEntry : collect.entrySet()) {
+                Long value = listEntry.getValue();
+                //根据条数合并单元格
+                if (value == 1) {
+                    //一条数据不合并
+                    for (ExcelHeader excelHeader : mergeList) {
+                        excelHeader.setLineNum(Math.toIntExact(excelHeader.getLineNum() + value));
+                    }
+                } else {
+                    System.out.println("合并列：合并单元列数==" + mergeList.size());
+                    //合并
+                    for (ExcelHeader excelHeader : mergeList) {
+                        System.out.println("合并列：合并行数==" + value);
+                        bigWriter.merge(excelHeader.getLineNum(),
+                                Math.toIntExact(excelHeader.getLineNum() + value - 1),
+                                excelHeader.getSortNum(), excelHeader.getSortNum(), null, true);
+                        excelHeader.setLineNum(Math.toIntExact(excelHeader.getLineNum() + value));
+                    }
+                }
             }
         }
     }
+
     /**
      * 设置Excel表头
      * @param clazz 映射实体
